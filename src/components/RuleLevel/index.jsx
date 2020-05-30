65f6b16956549012481042d6f53f59549bf16ca2
jcalhoon21 <jcalhoon21@gmail.com>
2020-05-30 10:09:31 -0700

    Setting up components

---
+++ src/components/RuleLevel/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './RuleLevelStyles.css';
+
+const RuleLevel = () => {
+  return(
+    <div>
+      <div>
+       High School Rules
+     </div>
+     <div>
+       NCAAW Rules
+     </div>
+     <div>
+       NCAAM Rules
+     </div>
+     <div>
+       NBA Rules
+     </div>
+    </div>
+  )
+}
+
+export default RuleLevel;
