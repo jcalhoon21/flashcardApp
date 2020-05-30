f6dea68e7bb5a04a38e0704f8a01cf660893b3db
jcalhoon21 <jcalhoon21@gmail.com>
2020-05-30 12:28:22 -0700

    Added NavBar and Footer components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './FooterStyles.css';
+
+const Footer = () => {
+  return(
+    <div>
+      <div className="footerContainer">
+        Ref Flashcards &copy; 2020
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBarStyles.css";
+
+const NavBar = () => {
+  return(
+    <div>
+      <div className="navContainer">
+        <div className="logo">Logo will go here</div>
+        <div className="menu">Home | Level | Account | About | Contact</div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar;
