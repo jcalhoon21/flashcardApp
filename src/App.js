d83d2448e7c14322ec212e688e0bb48db7f9ac0a
jcalhoon21 <jcalhoon21@gmail.com>
2020-06-01 17:34:05 -0700

    Added Flashcard and Flashcard questions, installed axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
-import React from 'react';
-import './App.css';
-import RuleLevel from '../src/components/RuleLevel';
+import React, { useState, useEffect } from "react";
+import NavBar from "../src/components/NavBar";
+import RuleLevel from "../src/components/RuleLevel";
+import Footer from '../src/components/Footer';
+import FlashcardList from '../src/components/FlashcardList';
+import axios from 'axios';
+import './app.css';
 
-function App() {
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2+2?',
+    answer: '4',
+    options: [
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+  {
+    id: 2,
+    question: 'Question 2',
+    answer: 'Answer',
+    options: [
+      "Answer 1",
+      "Answer 2",
+      "Answer",
+      "Answer 3"
+    ]
+  }
+]
+
+const App = () => {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10&category=21&difficulty=easy')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+          answer
+        ]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
     <div className="App">
-     <RuleLevel />
+      <NavBar />
+      <RuleLevel />
+      <FlashcardList flashcards={flashcards} />
+      <Footer />
     </div>
   );
 }
